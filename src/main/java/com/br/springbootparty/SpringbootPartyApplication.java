f88b408124899dda82dc3a0888f3ce776aa9f062
Fernandaaptoledo <fernanda-toledo2005@hotmail.com>
2023-03-08 15:57:53 -0300

    first commit

---
+++ src/main/java/com/br/springbootparty/SpringbootPartyApplication.java
@@ -0,0 +1,13 @@
+package com.br.springbootparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/springbootparty/SpringbootPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.springbootparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
